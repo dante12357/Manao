c374c47f4a3556540619c961ef65966b18a9265f
Максим Малькович <40199289+dante12357@users.noreply.github.com>
2020-08-15 16:18:18 +0300

    Update conf.php

--- App/config/conf.php
+++ App/config/conf.php
@@ -1,6 +1,5 @@
 <?php
 define("CONTROLLER_PATH", __DIR__ . "/../controller/");
-define("MODEL_PATH", __DIR__ . "../models/");
 
 require_once("route.php");
 require_once CONTROLLER_PATH . 'AbstractController.php';
